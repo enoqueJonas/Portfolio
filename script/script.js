6c5f78d1b4a14f77292afab8023b893e5dad9364
Enoque Jonas <enoquejonas@Enoques-MacBook-Air.local>
2022-10-11 19:30:43 +0200

    ADD href to the nav-link on the mobile style

--- script/script.js
+++ script/script.js
@@ -3,6 +3,9 @@ let close_menu_toolbar = document.querySelector('.close-menu-toolbar');
 let nav_menu = document.querySelector('.nav-menu');
 let btn_burger_menu = document.querySelector('.burger')
 let btn_close_menu = document.querySelector('.close-menu')
+let nav_item_portfolio = document.querySelector('#nav-item-portfolio');
+let nav_item_about = document.querySelector('#nav-item-about');
+let nav_item_contact = document.querySelector('#nav-item-contact');
 
 function toggleMobileMenu(){
     header.classList.toggle('active');
@@ -11,5 +14,8 @@ function toggleMobileMenu(){
     burger_menu.classList.toggle('active');
 }
 
+nav_item_contact.addEventListener('click', toggleMobileMenu)
+nav_item_about.addEventListener('click', toggleMobileMenu)
+nav_item_portfolio.addEventListener('click', toggleMobileMenu)
 btn_close_menu.addEventListener('click', toggleMobileMenu)
 btn_burger_menu.addEventListener('click', toggleMobileMenu)
