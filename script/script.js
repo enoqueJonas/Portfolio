a47be529db21f30a3fb6b97e9fefe09d0d4f48b8
Enoque Jonas <enoquejonas@Enoques-MacBook-Air.local>
2022-10-11 19:39:48 +0200

    FIX eslint errors

--- script/script.js
+++ script/script.js
@@ -1,21 +1,21 @@
-let header = document.querySelector('.header');
-let close_menu_toolbar = document.querySelector('.close-menu-toolbar');
-let nav_menu = document.querySelector('.nav-menu');
-let btn_burger_menu = document.querySelector('.burger')
-let btn_close_menu = document.querySelector('.close-menu')
-let nav_item_portfolio = document.querySelector('#nav-item-portfolio');
-let nav_item_about = document.querySelector('#nav-item-about');
-let nav_item_contact = document.querySelector('#nav-item-contact');
+const header = document.querySelector('.header');
+const closeMenuToolbar = document.querySelector('.close-menu-toolbar');
+const navMenu = document.querySelector('.nav-menu');
+const btnBurgerMenu = document.querySelector('.burger');
+const btnCloseMenu = document.querySelector('.close-menu');
+const navItemPortfolio = document.querySelector('#nav-item-portfolio');
+const navItemAbout = document.querySelector('#nav-item-about');
+const navItemContact = document.querySelector('#nav-item-contact');
 
-function toggleMobileMenu(){
-    header.classList.toggle('active');
-    close_menu_toolbar.classList.toggle('active');
-    nav_menu.classList.toggle('active');
-    burger_menu.classList.toggle('active');
+function toggleMobileMenu() {
+  header.classList.toggle('active');
+  closeMenuToolbar.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  btnBurgerMenu.classList.toggle('active');
 }
 
-nav_item_contact.addEventListener('click', toggleMobileMenu)
-nav_item_about.addEventListener('click', toggleMobileMenu)
-nav_item_portfolio.addEventListener('click', toggleMobileMenu)
-btn_close_menu.addEventListener('click', toggleMobileMenu)
-btn_burger_menu.addEventListener('click', toggleMobileMenu)
+navItemContact.addEventListener('click', toggleMobileMenu);
+navItemAbout.addEventListener('click', toggleMobileMenu);
+navItemPortfolio.addEventListener('click', toggleMobileMenu);
+btnCloseMenu.addEventListener('click', toggleMobileMenu);
+btnBurgerMenu.addEventListener('click', toggleMobileMenu);
