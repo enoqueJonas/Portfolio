6e38f02675ae8028830d916616d3a9a04e188077
Enoque Jonas <enoquejonas@Enoques-MacBook-Air.local>
2022-10-13 13:28:04 +0200

    ADD project cards popup window on mobile version

--- script/script.js
+++ script/script.js
@@ -6,7 +6,13 @@ const btnCloseMenu = document.querySelector('.close-menu');
 const navItemPortfolio = document.querySelector('#nav-item-portfolio');
 const navItemAbout = document.querySelector('#nav-item-about');
 const navItemContact = document.querySelector('#nav-item-contact');
-let projectCardsArray = [
+const btnCloseCardPopup = document.querySelector('.close-card-popup');
+
+// Section that holds all of the project cards
+const projectCardsSection = document.querySelector('#works');
+
+const cardsContainerPopupDiv = document.querySelector('.projects-popup');
+const projectCardsArray = [
   {
     name: 'Tonic',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
@@ -16,7 +22,7 @@ let projectCardsArray = [
     linkToSource: '#',
     canopy: 'Canopy',
     type: 'Back End Dev',
-    year: '2022'
+    year: '2022',
   },
   {
     name: 'Tonic',
@@ -27,7 +33,7 @@ let projectCardsArray = [
     linkToSource: '#',
     canopy: 'Canopy',
     type: 'Back End Dev',
-    year: '2022'
+    year: '2022',
   },
   {
     name: 'Tonic',
@@ -38,7 +44,7 @@ let projectCardsArray = [
     linkToSource: '#',
     canopy: 'Canopy',
     type: 'Back End Dev',
-    year: '2022'
+    year: '2022',
   },
   {
     name: 'Tonic',
@@ -49,119 +55,12 @@ let projectCardsArray = [
     linkToSource: '#',
     canopy: 'Canopy',
     type: 'Back End Dev',
-    year: '2022'
-  }
-]
+    year: '2022',
+  },
+];
 
-//Section that holds all of the project cards
-let projectCardsSection = document.querySelector('#works');
-
-function displayProjectCards() {
-  let counter = 0; 
-  //Adding the project cards info dynamically 
-  for (const project of projectCardsArray) {
-    counter++;
-    //Creating <p> that holds project year
-    let projectYear_P = document.createElement('p');
-    projectYear_P.className = 'works-item-project-info-year';
-    projectYear_P.textContent = project.year;
-
-    //Creating <img> that holds the bullet point for stylish purpose 
-    let projectInfoPoint_Img2 = document.createElement('img');
-    projectInfoPoint_Img2.className = 'works-item-project-info-point-2 works-item-project-info-items';
-    projectInfoPoint_Img2.src = 'img/Counter.png'
-    projectInfoPoint_Img2.alt = '...';
-
-    //Creating <span> and <p> that hold project type
-    let projectType_P = document.createElement('p');
-    project.className = 'works-item-project-info-type';
-    let projectType_Span = document.createElement('span')
-    projectType_Span.textContent = project.type;
-    projectType_P.appendChild(projectType_Span);
-
-    //Creating <img> that holds the bullet point for stylish purpose 
-    let projectInfoPoint_Img1 = document.createElement('img');
-    projectInfoPoint_Img1.className = 'works-item-project-info-point-1 works-item-project-info-items';
-    projectInfoPoint_Img1.src = 'img/Counter.png'
-    projectInfoPoint_Img1.alt = '...';
-
-    //Creating <span> and <p> that holds project Canopy 
-    let projectCanopy_P = document.createElement('p');
-    projectCanopy_P.classList = 'works-item-project-info-canopy works-item-project-info-items';
-    let projectCanopy_Span = document.createElement('span');
-    projectCanopy_Span.textContent = project.canopy;
-    projectCanopy_P.appendChild(projectCanopy_Span);
-
-    //Creating <div> that holds all of the above elements that are the project info and appending its children
-    let projectInfoFrame_Div = document.createElement('div');
-    projectInfoFrame_Div.className = 'works-item-project-info-frame';
-    projectInfoFrame_Div.appendChild(projectYear_P)
-    projectInfoFrame_Div.appendChild(projectInfoPoint_Img2)
-    projectInfoFrame_Div.appendChild(projectInfoPoint_Img1)
-    projectInfoFrame_Div.appendChild(projectType_P)
-    projectInfoFrame_Div.appendChild(projectCanopy_P)
-
-    //Creating <p> that holds the project description 
-    let projectDescription_P = document.createElement('p');
-    projectDescription_P.innerText = project.description;
-    projectDescription_P.className = 'works-item-project-description';
-
-    //Creating <li>'s and <span>'s that hold project tecnologies 
-    let html_Li = document.createElement('li');
-    let css_Li = document.createElement('li');
-    let javaScript_Li = document.createElement('li');
-    html_Li.className = 'works-item-project-tecnologies-item works-item-project-item-html';
-    let html_Span = document.createElement('span');
-    html_Span.textContent = project.technologies[0];
-    html_Li.appendChild(html_Span);
-
-    css_Li.className = 'works-item-project-tecnologies-item works-item-project-item-css';
-    let css_Span = document.createElement('span')
-    css_Span.textContent = project.technologies[1];
-    css_Li.appendChild(css_Span);
-
-    javaScript_Li.className = 'works-item-project-tecnologies-item works-item-project-item-css';
-    let javaScript_Span = document.createElement('span');
-    javaScript_Span.textContent = project.technologies[2];
-    javaScript_Li.appendChild(javaScript_Span);
-
-    //Creating the <ul> that holds all of the above <li> and appending its children
-    let projectTecnologies_Ul = document.createElement('ul');
-    projectTecnologies_Ul.className = 'works-item-project-tecnologies';
-    projectTecnologies_Ul.appendChild(html_Li);
-    projectTecnologies_Ul.appendChild(css_Li);
-    projectTecnologies_Ul.appendChild(javaScript_Li);
-
-    //Creating the card <div> that holds the button and the <button>
-    let porjectButton_Btn = document.createElement('button');
-    porjectButton_Btn.textContent = 'See Project'
-    porjectButton_Btn.className = 'works-item-project-action-button';
-    
-    let projectButton_Div = document.createElement('div');
-    projectButton_Div.id = 'works-item-project-action';
-    projectButton_Div.appendChild(porjectButton_Btn);
-
-    //Creating the <div> that is the project card
-    let projectCardInfoBlock_Div = document.createElement('div');
-    projectCardInfoBlock_Div.className = 'works-item-info-block';
-    projectCardInfoBlock_Div.appendChild(projectDescription_P);
-    projectCardInfoBlock_Div.appendChild(projectTecnologies_Ul);
-    projectCardInfoBlock_Div.appendChild(projectInfoFrame_Div);
-    projectCardInfoBlock_Div.appendChild(projectButton_Div);
-
-    //Creating the <div> that holds the image
-    let projectImg_Div = document.createElement('div');
-    projectImg_Div.className = project.img + ' works-item-image'
-
-    //Creating the project card main <div> that holds all the card info and appending its children
-    let projectCard_Div = document.createElement('div');
-    projectCard_Div.className = 'works-item works-item-'+counter;
-    projectCard_Div.appendChild(projectImg_Div);
-    projectCard_Div.appendChild(projectCardInfoBlock_Div);
-
-    //Appending the current project card int the section that holds all project cards 
-    projectCardsSection.appendChild(projectCard_Div);
-  }
+function toggleProjectCardsPopup() {
+  cardsContainerPopupDiv.classList.toggle('active-popup');
 }
 
 function toggleMobileMenu() {
@@ -171,6 +70,117 @@ function toggleMobileMenu() {
   btnBurgerMenu.classList.toggle('active');
 }
 
+function displayProjectCards() {
+  // Adding the project cards info dynamically
+  for (const project of projectCardsArray) {
+    // Creating <p> that holds project year
+    const projectYearP = document.createElement('p');
+    projectYearP.className = 'works-item-project-info-year';
+    projectYearP.textContent = project.year;
+
+    // Creating <img> that holds the bullet point for stylish purpose
+    const projectInfoPointImg2 = document.createElement('img');
+    projectInfoPointImg2.className = 'works-item-project-info-point-2 works-item-project-info-items';
+    projectInfoPointImg2.src = 'img/Counter.png';
+    projectInfoPointImg2.alt = '...';
+
+    // Creating <span> and <p> that hold project type
+    const projectTypeP = document.createElement('p');
+    project.className = 'works-item-project-info-type';
+    const projectTypeSpan = document.createElement('span');
+    projectTypeSpan.textContent = project.type;
+    projectTypeP.appendChild(projectTypeSpan);
+
+    // Creating <img> that holds the bullet point for stylish purpose
+    const projectInfoPointImg1 = document.createElement('img');
+    projectInfoPointImg1.className = 'works-item-project-info-point-1 works-item-project-info-items';
+    projectInfoPointImg1.src = 'img/Counter.png';
+    projectInfoPointImg1.alt = '...';
+
+    // Creating <span> and <p> that holds project Canopy
+    const projectCanopyP = document.createElement('p');
+    projectCanopyP.classList = 'works-item-project-info-canopy works-item-project-info-items';
+    const projectCanopySpan = document.createElement('span');
+    projectCanopySpan.textContent = project.canopy;
+    projectCanopyP.appendChild(projectCanopySpan);
+
+    // Creating <div> that holds all of the above
+    // elements that are the project info and appending its children
+    let projectInfoFrameDiv = '';
+    projectInfoFrameDiv = document.createElement('div');
+    projectInfoFrameDiv.className = 'works-item-project-info-frame';
+    projectInfoFrameDiv.appendChild(projectYearP);
+    projectInfoFrameDiv.appendChild(projectInfoPointImg2);
+    projectInfoFrameDiv.appendChild(projectInfoPointImg1);
+    projectInfoFrameDiv.appendChild(projectTypeP);
+    projectInfoFrameDiv.appendChild(projectCanopyP);
+
+    // Creating <p> that holds the project description
+    let projectDescriptionP = '';
+    projectDescriptionP = document.createElement('p');
+    projectDescriptionP.innerText = project.description;
+    projectDescriptionP.className = 'works-item-project-description';
+
+    // Creating <li>'s and <span>'s that hold project tecnologies
+    const htmlLi = document.createElement('li');
+    const cssLi = document.createElement('li');
+    const javaScriptLi = document.createElement('li');
+    htmlLi.className = 'works-item-project-tecnologies-item works-item-project-item-html';
+    const htmlSpan = document.createElement('span');
+    htmlSpan.textContent = project.technologies[0];
+    htmlLi.appendChild(htmlSpan);
+
+    cssLi.className = 'works-item-project-tecnologies-item works-item-project-item-css';
+    const cssSpan = document.createElement('span');
+    cssSpan.textContent = project.technologies[1];
+    cssLi.appendChild(cssSpan);
+
+    javaScriptLi.className = 'works-item-project-tecnologies-item works-item-project-item-css';
+    const javaScriptSpan = document.createElement('span');
+    javaScriptSpan.textContent = project.technologies[2];
+    javaScriptLi.appendChild(javaScriptSpan);
+
+    // Creating the <ul> that holds all of the above <li> and appending its children
+    const projectTecnologiesUl = document.createElement('ul');
+    projectTecnologiesUl.className = 'works-item-project-tecnologies';
+    projectTecnologiesUl.appendChild(htmlLi);
+    projectTecnologiesUl.appendChild(cssLi);
+    projectTecnologiesUl.appendChild(javaScriptLi);
+
+    // Creating the card <div> that holds the button and the <button>
+    const porjectButtonBtn = document.createElement('button');
+    porjectButtonBtn.textContent = 'See Project';
+    porjectButtonBtn.className = 'works-item-project-action-button';
+    porjectButtonBtn.addEventListener('click', toggleProjectCardsPopup);
+
+    const projectButtonDiv = document.createElement('div');
+    projectButtonDiv.id = 'works-item-project-action';
+    projectButtonDiv.appendChild(porjectButtonBtn);
+
+    // Creating the <div> that is the project card
+    const projectCardInfoBlockDiv = document.createElement('div');
+    projectCardInfoBlockDiv.className = 'works-item-info-block';
+    projectCardInfoBlockDiv.appendChild(projectDescriptionP);
+    projectCardInfoBlockDiv.appendChild(projectTecnologiesUl);
+    projectCardInfoBlockDiv.appendChild(projectInfoFrameDiv);
+    projectCardInfoBlockDiv.appendChild(projectButtonDiv);
+
+    // Creating the <div> that holds the image
+    const projectImgDiv = document.createElement('div');
+    projectImgDiv.className = `${project.img} works-item-image`;
+
+    // Creating the project card main <div> that holds all the card info and appending its children
+    const projectCardDiv = document.createElement('div');
+    projectCardDiv.className = `works-item works-item-${projectCardsArray.entries()}`;
+    projectCardDiv.appendChild(projectImgDiv);
+    projectCardDiv.appendChild(projectCardInfoBlockDiv);
+
+    // Appending the current project card int the section that holds all project cards
+    projectCardsSection.appendChild(projectCardDiv);
+  }
+}
+
+btnCloseCardPopup.addEventListener('click', toggleProjectCardsPopup);
 window.addEventListener('load', displayProjectCards);
 navItemContact.addEventListener('click', toggleMobileMenu);
 navItemAbout.addEventListener('click', toggleMobileMenu);
