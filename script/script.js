0f5c4b6cd8763481489feb62ebceec10c9f8aa4b
enoqueJonas <enoque.macanda@gmail.com>
2023-07-07 20:08:57 +0200

    UPDATE header text

--- script/script.js
+++ script/script.js
@@ -25,7 +25,7 @@ var sticky = navbar.offsetTop;
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function controlScroll() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
   }
